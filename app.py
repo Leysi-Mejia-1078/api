21a10284737134baef92b2c3b46c40c41796ca7e
Angel Perez <a.22308051281086@gmail.com>
2025-05-21 21:46:55 -0600

    cambio de imagenes

--- app.py
+++ app.py
@@ -18,14 +18,14 @@ def insertar_productos():
             "descripcion": "Cuaderno de 100 hojas, rayado",
             "precio": 25.50,
             "rating": 4.7,
-            "imagen": "/static/cuaderno.jpg"
+            "imagen": "https://raw.githubusercontent.com/Angel-Perez-1086/imagenes_github/refs/heads/main/pack%20lapiz.jpg"
         },
         {
             "nombre": "Bolígrafos azul (pack x10)",
             "descripcion": "Bolígrafos de tinta azul, punta fina",
             "precio": 40.00,
             "rating": 4.3,
-            "imagen": "/static/boligrafos.jpg"
+            "imagen": "https://raw.githubusercontent.com/Angel-Perez-1086/imagenes_github/refs/heads/main/pack%20lapiz.jpg"
         },
         # Agrega más productos si lo deseas
     ]
